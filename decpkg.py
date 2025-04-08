55ee5ee2a3b262cfe7a7f9430dafcdeb1fe2c0dd
maaru <maratarzymatov288@gmail.com>
2025-04-08 18:59:39 +0600

    update

--- decpkg.py
+++ decpkg.py
@@ -25,6 +25,7 @@
 
 import os
 import subprocess
+from time import sleep
 import argcomplete
 import pathlib
 import argparse
@@ -67,6 +68,7 @@ BACK_WHITE = "\033[47m"
 
 
 def main():
+    ascii_art()
     if args.aur:
         Aur_helper_soft()
     elif args.version:
@@ -84,6 +86,9 @@ class DecpkgSync:
         self.root = self.root_status()
         self.noconfirm = self.set_noconfirm()
         self.aur_helper = self.get_aur_helper()
+        self.notify = self.get_notify()
+        if self.state_update():
+            UpdateSystem()
 
         if self.relative and self.absolute:
             print(
@@ -110,6 +115,19 @@ class DecpkgSync:
                     f"\n {YELLOW}~~~  select at least 1 installation\n option relative or absolute write true is manual  :( \n"
                 )
                 return sys.exit(0)
+
+            if self.notify:
+                subprocess.run(["notify-send", f"deckpkg Sync"])
+
+    def state_update(self) -> bool:
+        obj = UpdateSystem.__new__(UpdateSystem)
+        state = obj.get_state_update()
+        return state
+
+    def get_notify(self) -> bool:
+        obj = UpdateSystem.__new__(UpdateSystem)
+        state = obj.get_notify()
+        return state
 
     def relative_sync_install_pkg(self):
         jsons = self.read_json_without_comments()
@@ -157,10 +175,12 @@ class DecpkgSync:
 
         for i in haven_list:
             print(i)
+            self.notify_send(f"haven pkg list: {len(haven_list)}")
 
+        sleep(0.5)
         for i in aur_haven_list:
             print(i)
-
+            self.notify_send(f"AUR haven pkg list: {len(aur_haven_list)}", r="9998")
         print("▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁")
 
         for i in nohaven_list:
@@ -172,12 +192,19 @@ class DecpkgSync:
         print()
 
         for pkg in nohaven_list:
-            command = f"{self.root} pacman -Syu {pkg} {self.noconfirm}"
+            command = f"{self.root} pacman -S {pkg} {self.noconfirm}"
             os.system(command)
 
         for pkg in aur_nohaven_list:
-            command = f"{aurhelper} -Syu {pkg} {self.noconfirm}"
+            command = f"{aurhelper} -Sa {pkg} {self.noconfirm}"
             os.system(command)
+
+    def notify_send(self, command: str, r: str = "9999") -> None:
+        """notify-send {command}"""
+        if self.notify:
+            subprocess.run(
+                f"notify-send -t 4000 -r {r if r else ''} '{command}'", shell=True
+            )
 
     def get_aur_helper(self) -> str:
         try:
@@ -185,7 +212,7 @@ class DecpkgSync:
             aur = aur.get("aur_helper", "")
             return aur
         except KeyError:
-            print("\n{YELLOW} ~~~>  'aur_helper' not found in config.")
+            print(f"\n{YELLOW} ~~~>  '{self.aur_helper}' not found in config.")
             return ""
 
     def set_noconfirm(self) -> str:
@@ -305,13 +332,227 @@ class DecpkgSync:
 
         return value
 
-    def read_json_without_comments(self):
+    def read_json_without_comments(self) -> dict:
         with open(CONFIG_JSON, "r") as f:
             jsonc_content = f.read()
             jsonc_content = re.sub(r"//.*", "", jsonc_content)
             jsonc_content = re.sub(r",\s*(]|})", r"\1", jsonc_content)
             data = json.loads(jsonc_content)
             return data
+
+
+def ascii_art() -> str:
+    obj = DecpkgSync.__new__(DecpkgSync)
+    aur = obj.read_json_without_comments()
+    data = aur
+    value = data.get("logo_ascii", False)
+    if not isinstance(value, bool):
+        value = False
+    if value:
+        logo = """
+▓█████▄ ▓█████  ▄████▄   ██▓███   ██ ▄█▀  ▄████
+▒██▀ ██▌▓█   ▀ ▒██▀ ▀█  ▓██░  ██▒ ██▄█▒  ██▒ ▀█▒
+░██   █▌▒███   ▒▓█    ▄ ▓██░ ██▓▒▓███▄░ ▒██░▄▄▄░
+░▓█▄   ▌▒▓█  ▄ ▒▓▓▄ ▄██▒▒██▄█▓▒ ▒▓██ █▄ ░▓█  ██▓
+░▒████▓ ░▒████▒▒ ▓███▀ ░▒██▒ ░  ░▒██▒ █▄░▒▓███▀▒
+ ▒▒▓  ▒ ░░ ▒░ ░░ ░▒ ▒  ░▒▓▒░ ░  ░▒ ▒▒ ▓▒ ░▒   ▒
+ ░ ▒  ▒  ░ ░  ░  ░  ▒   ░▒ ░     ░ ░▒ ▒░  ░   ░
+ ░ ░  ░    ░   ░        ░░       ░ ░░ ░ ░ ░   ░
+   ░       ░  ░░ ░               ░  ░         ░
+ ░             ░
+            """
+        return f"{print(logo)}"
+    else:
+        return ""
+
+
+class UpdateSystem:
+    def __init__(self) -> None:
+        self.notify = self.get_notify()
+        self.aur_helper = self.get_aur_helper()
+        self.noconfirm = self.get_noconfirm()
+        self.state_update = self.get_state_update()
+
+        self.state_pacman = self.get_state_pacman_update()
+        self.state_aur = self.get_state_aur_update()
+        self.state_flatpak = self.get_state_flatpak_update()
+
+        self.status_counter = self.get_state_update_counter()
+        self.main()
+
+    def get_notify(self):
+        data = self.get_read_json_without_comments()
+        value = data.get("notify", False)
+        if not isinstance(value, bool):
+            value = False
+        return value
+
+    def get_state_update(self) -> bool:
+        data = self.get_read_json_without_comments()
+        value = data.get("update_pkg_with_install", False)
+        if not isinstance(value, bool):
+            value = False
+        return value
+
+    def get_state_update_counter(self) -> bool:
+        data = self.get_read_json_without_comments()
+        value = data.get("update_counter", False)
+        if not isinstance(value, bool):
+            value = False
+        return value
+
+    def get_state_flatpak_update(self) -> bool:
+        data = self.get_read_json_without_comments()
+        value = data.get("update_flatpak", False)
+        if not isinstance(value, bool):
+            value = False
+        return value
+
+    def get_state_aur_update(self) -> bool:
+        data = self.get_read_json_without_comments()
+        value = data.get("update_aur", False)
+        if not isinstance(value, bool):
+            value = False
+        return value
+
+    def get_state_pacman_update(self) -> bool:
+        data = self.get_read_json_without_comments()
+        value = data.get("update_pacman", False)
+        if not isinstance(value, bool):
+            value = False
+        return value
+
+    def get_read_json_without_comments(self) -> dict:
+        obj = DecpkgSync.__new__(DecpkgSync)
+        aur = obj.read_json_without_comments()
+        return aur
+
+    def get_aur_helper(self) -> str:
+        obj = DecpkgSync.__new__(DecpkgSync)
+        aur = obj.get_aur_helper()
+        return f"{aur}"
+
+    def get_noconfirm(self) -> str:
+        obj = DecpkgSync.__new__(DecpkgSync)
+        noconfirm = obj.set_noconfirm()
+        return f"{noconfirm}"
+
+    def main(self) -> None:
+        self.counter_ui()
+        self.start_update()
+
+    def shell(self, command) -> str:
+        return subprocess.getoutput(command)
+
+    def ending(self):
+        print(
+            f"{RESET}▁" * 40 + f"{RESET}\n\n"
+            f"{YELLOW}\n === Done ==="
+            f"{RESET}\n\n" + "▁" * 40 + f"{RESET}\n\n"
+        )
+        sleep(5)
+
+    def start_update(self):
+        if self.state_update:
+            if self.notify:
+                self.shell(f"notify-send ' Updating !!! \nSudo password needed'")
+
+            if self.state_pacman:
+                os.system(f"sudo pacman -Syu {self.noconfirm}")
+
+            elif self.state_aur:
+                self.update_wtih_aur_helper()
+
+            elif self.state_flatpak:
+                self.flatpak_update()
+
+            self.ending()
+
+            if self.notify:
+                subprocess.run(["notify-send", f"Update Ending"])
+        else:
+            return
+
+    def update_wtih_aur_helper(self) -> int | None:
+        if not self.shell(f"command -v {self.aur_helper}"):
+            print(f"{self.aur_helper} not found")
+            return
+        return os.system(f"{self.aur_helper} -Syu {self.noconfirm}")
+
+    def flatpak_update(self) -> int | None:
+        if not self.shell("command -v flatpak"):
+            print("flatpak not found")
+            return
+        return os.system("flatpak update")
+
+    def counter_ui(self):
+        if self.status_counter:
+            output = []
+            if self.state_pacman:
+                output.append(
+                    f"{YELLOW}~~>   󰮯  :  Pacman   ===     {self.check_pacman()}{RESET}"
+                )
+            if bool(self.state_aur):
+                output.append(
+                    f"{BLUE}~~>     :  {self.aur_helper}      ===     {self.check_aur_helper()}{RESET}"
+                )
+            if self.state_flatpak:
+                output.append(
+                    f"{CYAN}~~>     :  Flatpak  ===     {self.check_flatpak()}{RESET}"
+                )
+
+            output.append(
+                f"{RESET}▁" * 40
+                + f"\n\n {GREEN}=== Updates : {self.sum_updates()} ==={RESET}\n"
+            )
+            print("\n".join(output))
+
+            if self.notify:
+                subprocess.run(["notify-send", f"Updates : {self.sum_updates()}"])
+
+    def get_check_pacman(self) -> int:
+        if not self.shell("command -v checkupdates"):
+            return 0
+
+        attempt = 0
+        while attempt < 5:
+            result = self.shell("checkupdates | wc -l")
+            if "ERROR: Cannot fetch updates" in result or result.strip() == "":
+                attempt += 1
+            else:
+                return int(result) if result.strip().isdigit() else 0
+
+        print(YELLOW, f" Pacman could not update", RESET)
+        return 0
+
+    def check_pacman(self) -> int:
+        return self.get_check_pacman()
+
+    def check_aur_helper(self) -> int:
+        if not self.shell(f"command -v {self.aur_helper}"):
+            return 0
+        updates = self.shell(f"{self.aur_helper} -Qua | wc -l")
+        return int(updates) if updates.strip().isdigit() else 0
+
+    def check_flatpak(self) -> int:
+        if not self.shell("command -v flatpak"):
+            return 0
+        updates = self.shell("flatpak remote-ls --updates | wc -l")
+        return int(updates) if updates.strip().isdigit() else 0
+
+    def sum_updates(self):
+        total = 0
+        if self.state_pacman:
+            total += self.check_pacman()
+        if self.state_aur:
+            total += self.check_aur_helper()
+        if self.state_flatpak:
+            total += self.check_flatpak()
+
+        if self.get_notify:
+            self.shell(f"notify-send ' Updates : {total}'")
+
+        return total
 
 
 class GenerateConfigure:
@@ -340,23 +581,28 @@ class GenerateConfigure:
   //
   //  ------------------------------------------------
   {
-    // Startup
-    "absolute_sync": false, // absolute installation where you clearly specify what to remove and what to install if a package is not in the list, then it will be removed. 
-    "relative_sync": true,  // Relative package installation it you can determine which packages to install but not remove, If you installed through the carrier option you will have to uninstall manually. 
-    "noconfirm" : true, // If that's true, then the packets will settle without confirmation.  
+  // Startup
+  "logo_ascii"   : true , // there will be a logo at the beginning. 
+  "absolute_sync": false, // absolute installation where you clearly specify what to remove and what to install if a package is not in the list, then it will be removed. 
+  "relative_sync": true , // Relative package installation it you can determine which packages to install but not remove, If you installed through the carrier option you will have to uninstall manually. 
+  "noconfirm"    : true , // If that's true, then the packets will settle without confirmation.  
 
-    // Updates package
-    "update_pkg_with_install": false, // You can upgrade your system before you start installing your packages.
-    "update_counter": false, // If you choose to update the system before installation you can see the number of updates. 
+  // Updates package
+  "update_pkg_with_install": false, // You can upgrade your system before you start installing your packages.
+  "update_counter"         : false, // If you choose to update the system before installation you can see the number of updates. 
 
-    // set root
-    "use_root": "sudo",  // To install packages you need to have super user rights in order to install them. 
+  "update_pacman" : true,  // allow pacman to be updated first  
+  "update_aur"    : true,  // allow Arch User Rep to be updated first 
+  "update_flatpak": false, // allow flatpak to be updated first
 
-    // aur 
-    "aur_helper": "paru", // Better aur helper utils.
+  // set root
+  "use_root"  : "sudo",  // To install packages you need to have super user rights in order to install them. 
 
-    // other
-    "notify": false // If you want to be notified.
+  // aur 
+  "aur_helper": "paru",  // Better aur helper utils.
+
+  // other
+  "notify"    : false,   // If you want to be notified.
   },
   {
     "pacman": [
